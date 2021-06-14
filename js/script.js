0a124a7559ca3397dfc0758cfaa4b2cb9ed46f82
Richard Blaauw <richard.blaauw@ogd.nl>
2021-06-14 19:38:19 +0200

    Add code that rewinds audio to 0, so that on each keypress, the audio starts again

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // Listen for keydown event on the window
 window.addEventListener('keydown', function (event) {
   let audio = document.querySelector(`audio[data-key="${event.key}"]`); //keyCode is deprecated
-  if (!audio) return; //stop function from running
+  if (!audio) return; // stop function from running
+  audio.currentTime = 0; // rewinds audio to 0 --> this allows for keymashing, otherwise the audio won't play if it is already playing
   audio.play();
 });
