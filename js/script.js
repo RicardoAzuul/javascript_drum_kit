83772e82ef74ef24a80fd2c91ca9517ab90c8c26
Richard Blaauw <richard.blaauw@ogd.nl>
2021-06-14 19:44:25 +0200

    Update data-key for the div key elements, add code that selects div that has the right key as attribute.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 // Listen for keydown event on the window
 window.addEventListener('keydown', function (event) {
   let audio = document.querySelector(`audio[data-key="${event.key}"]`); //keyCode is deprecated
+  let key = document.querySelector(`.key[data-key="${event.key}"]`);
   if (!audio) return; // stop function from running
   audio.currentTime = 0; // rewinds audio to 0 --> this allows for keymashing, otherwise the audio won't play if it is already playing
   audio.play();
+  console.log(key);
 });
